244c8815da26c7ea4b7bc163e792c8b41554d118
Nicholas <Nick_3721@hotmail.com>
2024-07-18 23:21:42 +0100

    dynamic version of game implemented
    Game now has a 3 turn limit, where the oldest piece is removed.
    
    Issues still to fix:
    - checkWin needs to be fired inside of  if else statements of turn
    - can currently replace pieces on the board by clicking over them
    
    to be added:
    - fireworks for winner
    - revert to original version?
    - reset button
    - game score tally

--- src/Components/TacTacToe/TicTacToe.jsx
+++ src/Components/TacTacToe/TicTacToe.jsx
@@ -1,28 +1,88 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect  } from 'react'
 import './TicTacToe.css'
 import circleIcon from '../Assets/circle.png'
 import crossIcon from '../Assets/cross.png'
 
-let data = ["","","","","","","","",""];
 
 const TicTacToe = () => {
   let [count, setCount] = useState(0);
-  let [lock, setLock] = useState(false);
-  let titleRef = useRef(null)
+  let [lock, setLock] = useState(false);  
+
+  const boxRefs = useRef(Array.from({ length: 9 }, () => React.createRef()));
+
+  let [X_turn, setX_Turn] = useState(true)
+  const [X_Positions, setX_Positions] = useState([])
+  const [O_Positions, setO_Positions] = useState([])
+  
+  const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],  // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],  // Columns
+    [0, 4, 8], [2, 4, 6]              // Diagonals
+  ];
+  
+  const handleTurn = (position) => {
+    if (lock) { return }
+
+    if (X_turn) {
+      const newX_Positions = [...X_Positions];
+      
+      if (newX_Positions.length === 3) {
+        const removedXPosition = newX_Positions.shift();
+        newX_Positions.push(position);
+        boxRefs.current[removedXPosition].current.innerHTML = ''
+      } else {
+        newX_Positions.push(position);
+      }
+      setX_Positions(newX_Positions);
+      
+      draw(newX_Positions, "X");  
+      setX_Turn(false)
+    } 
+
+    else {
+      const newO_Positions = [...O_Positions];
+      
+      if (newO_Positions.length === 3) {
+        const removedOPosition = newO_Positions.shift();
+        newO_Positions.push(position);
+        boxRefs.current[removedOPosition].current.innerHTML = ''
+      } else { 
+        newO_Positions.push(position);
+      }
+      setO_Positions(newO_Positions);
+
+      draw(newO_Positions, "O");  
+      setX_Turn(true)
+    }
+    checkWin(X_Positions, "X")
+    checkWin(O_Positions, "O")
+  }
+  
+
+  function draw(gamePieces, side) {
+    for (let i = 0; i < gamePieces.length; i++) {
+      boxRefs.current[gamePieces[i]].current.classList.remove('vanishing')
+      side === "X" 
+        ? boxRefs.current[gamePieces[i]].current.innerHTML = `<img src='${crossIcon}' alt='X' />`
+        : boxRefs.current[gamePieces[i]].current.innerHTML = `<img src='${circleIcon}' alt='O' />`;
+    }
+    if (gamePieces.length === 3) {
+      const oldestXPosition = gamePieces[0];
+      boxRefs.current[oldestXPosition].current.classList.add('vanishing')
+  }
+}
+
+
+  useEffect(() => {
+    console.log(`X positions are: ${X_Positions}`);
+  }, [X_Positions]);
+
+  useEffect(() => {
+    console.log(`Y positions are: ${O_Positions}`);
+  }, [O_Positions]);
 
 
 
-  let box1 = useRef(null);
-  let box2 = useRef(null);
-  let box3 = useRef(null);
-  let box4 = useRef(null);
-  let box5 = useRef(null);
-  let box6 = useRef(null);
-  let box7 = useRef(null);
-  let box8 = useRef(null);
-  let box9 = useRef(null);
-
- let boxArray = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
 
   const handleClick = (e, num) => {
     if(lock) {
@@ -38,82 +98,37 @@ const TicTacToe = () => {
       data[num]="o";
       setCount(++count);
     }
-    checkWin();
+
   }
  
-  const checkWin = () => {
-    if(data[0]===data[1] && data[1]===data[2] && data[2]!== "") {
-      gameWon(data[2])
-    }
-    else if(data[3]===data[4] && data[4]===data[5] && data[5]!== "") {
-      gameWon(data[5])
-    }
-    else if(data[6]===data[7] && data[7]===data[8] && data[8]!== "") {
-      gameWon(data[8])
-    }
-    else if(data[0]===data[3] && data[3]===data[6] && data[6]!== "") {
-      gameWon(data[6])
-    }
-    else if(data[1]===data[4] && data[4]===data[7] && data[7]!== "") {
-      gameWon(data)
-    }
-    else if(data[2]===data[5] && data[5]===data[8] && data[8]!== "") {
-      gameWon(data[8])
-    }
-    else if(data[0]===data[4] && data[4]===data[8] && data[8]!== "") {
-      gameWon(data[8])
-    }
-    else if(data[2]===data[4] && data[4]===data[6] && data[6]!== "") {
-      gameWon(data[6])
-    }
-  } 
+  const checkWin = (positions, currentPlayer) => {
+    const hasWon = winningCombinations.some(combination =>
+      combination.every(index => positions.includes(index))
+    );
   
-  const gameWon = (winner) => {
-    console.log("game won by:", winner)
-    setLock(true);
-    if(winner==='x') {
-      titleRef.current.innerHTML = `Congratulations: <img src='${crossIcon}'> wins`
+    if (hasWon) {
+      alert(`${currentPlayer} wins!`);
+      setLock(true);
     }
-    else if (winner=== 'o') {
-      titleRef.current.innerHTML = `Congratulations: <img src='${circleIcon}'> wins`
-    }
-  }
-
-
-
-  const resetHandleClick = () => {
-    setLock(false)
-    data = ["","","","","","","","",""];
-    titleRef.current.innerHTML = 'Tic tac Toe Game in <span>&nbsp;React</span>'
-
-    boxArray.map((e) => {
-      e.current.innerHTML = ''
-    })
-  }
-
-
+  
+    return hasWon;
+  };
+ 
+ 
   return (
-    <div className='container'>
-      <h1 className="title" ref={titleRef}>Tic tac Toe Game in <span>&nbsp;React</span></h1>
-      <div className="board">
-        <div className="row1">
-          <div className="boxes" ref={box1} onClick={(e)=>{handleClick(e,0)}}></div>
-          <div className="boxes" ref={box2} onClick={(e)=>{handleClick(e,1)}}></div>
-          <div className="boxes" ref={box3} onClick={(e)=>{handleClick(e,2)}}></div>
-        </div>
-        <div className="row2">
-          <div className="boxes" ref={box4} onClick={(e)=>{handleClick(e,3)}}></div>
-          <div className="boxes" ref={box5} onClick={(e)=>{handleClick(e,4)}}></div>
-          <div className="boxes" ref={box6} onClick={(e)=>{handleClick(e,5)}}></div>
-        </div>
-        <div className="row3">
-          <div className="boxes" ref={box7} onClick={(e)=>{handleClick(e,6)}}></div>
-          <div className="boxes" ref={box8} onClick={(e)=>{handleClick(e,7)}}></div>
-          <div className="boxes" ref={box9} onClick={(e)=>{handleClick(e,8)}}></div>
-        </div>
-      </div>
-      <button className='reset' onClick={()=>{resetHandleClick()}}>Reset</button>
-    </div>
+    <div className="board">
+    {Array.from({ length: 9 }).map((_, index) => (
+      <div
+        key={index}
+        className="boxes"
+        ref={boxRefs.current[index]}
+        onClick={(e) => handleTurn(index)}
+      ></div>
+    ))}
+  </div>
+
+
+
   )
 }
 
